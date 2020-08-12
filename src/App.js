5f3d3751a423c1984693d774953b14a7988f4c36
sang970608 <sang970608@gmail.com>
2020-08-12 20:06:25 +0900

    contact 내용 추가, animation 추가 및 shadow 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Js from './js.jpg';
 import reacts from './react.png';
 import bgm from './bgm.PNG';
 import escape from './escape.png';
+import bucheon from './bucheon.jpg'
 
 
 function Home(){
@@ -83,20 +84,72 @@ function Project() {
       </div>
       <div className="projectImg">
        <div className="firstProject">
-         <img src={bgm} alt="브금 사이트" className="bgm" />
-         <div className="bgmExplain">
-           <h3>BGM ON</h3>
-           <div className="bgmText">
-           듣고 싶은 노래를 바로 듣자!
-           게임 도중 또는 
-
+         <a href="https://github.com/sang970608/bgm" target="_blank" >
+         <img src={bgm} alt="브금 사이트" className="bgm" /></a>
+         <div className="proText">
+          <div className="proInfo">
+            <h3>BGM ON</h3>
+            게임 도중에 듣고 싶은 노래를
+            바로 듣자!
+           </div>
+           <div className="proDev">
+             <strong>나의 역할</strong>
+             <br />
+             디자인
+             <br />
+             퍼블리싱
+             <br />
+             유튜브 링크 사용
            </div>
          </div>
+         
        </div>
         <div className="secondProject">
-          <img src={escape} alt="방탈출" className="escape" />
+          <a href="https://github.com/sang970608/escape" target="_blank">
+          <img src={escape} alt="방탈출" className="escape" /></a>
+          <div className="proText">
+            <div className="proInfo">
+              <h3>학교 탈출 !</h3>
+              학교를 탈출하면서 정보보안 및 정처기 문제를 풀자!
+            </div>
+            <div className="proDev">
+              <strong>나의 역할</strong>
+              <br />
+              메뉴 및 게임화면 개발
+              <br />
+              로그인 화면 개발
+              <br />
+              소셜 로그인 api 사용
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  )
+}
+
+function Contact() {
+  return(
+    <div className="contact">
+      <div className="contactSub">
+        <h1 className="contactH1">Contact</h1>
+      </div>
+      <div className="card">
+        <div className="cardLeft">
+          <strong>박상원 Park Sang Won</strong> | student<br />
+          MOBILE : 010-6510-0608<br />
+          EMAIL : <a className="contactLink" href="mailto:sang970608@naver.com?Subject=포트폴리오보고 연락드립니다.">
+          <strong>sang970608@naver.com</strong></a><br />
+          Insta : <a className="contactLink" href="https://www.instagram.com/sang970608/" >
+          <strong>@sang970608</strong></a><br />
+          <hr /><br />
+          통화 가능 시간  <strong>11:00 ~ 23:00</strong>
+        </div>
+        <div className="cardRight">
+          <img src={bucheon} alt="학교아이콘" className="cardImg" />
+        </div>
+      </div>
+      <div className="full"></div>
     </div>
   )
 }
@@ -132,7 +185,7 @@ function App() {
         <Route exact path="/"><Home /></Route>
         <Route path="/About"><About /></Route>
         <Route path="/Project"><Project /></Route>
-        <Route path="/Contact">연락처</Route>
+        <Route path="/Contact"><Contact /></Route>
         <Route path="/">잘못 들어오셨습니다.</Route>
       </Switch>
       <Footer />
