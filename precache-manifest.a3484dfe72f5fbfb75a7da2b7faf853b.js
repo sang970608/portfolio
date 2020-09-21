331c3a890ce802a3c9880da333c38d4e77fe6d97
sang970608 <sang970608@naver.com>
2020-09-21 17:56:58 +0900

    Updates

---
+++ precache-manifest.a3484dfe72f5fbfb75a7da2b7faf853b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7047aca7f3ba99b60af8dbf73602592",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "43154e755e9253816b58",
+    "url": "/portfolio/static/css/main.06f399b6.chunk.css"
+  },
+  {
+    "revision": "c9e07c3f039db972e027",
+    "url": "/portfolio/static/js/2.44c55612.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.44c55612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43154e755e9253816b58",
+    "url": "/portfolio/static/js/main.ce2eb4e6.chunk.js"
+  },
+  {
+    "revision": "30c7e0067c4da7d72fec",
+    "url": "/portfolio/static/js/runtime-main.18c1d51b.js"
+  },
+  {
+    "revision": "9876408727726cee8a7dd9d288e6d091",
+    "url": "/portfolio/static/media/bgm.98764087.PNG"
+  },
+  {
+    "revision": "22f66603238fe7a3c1daea23e3c252d6",
+    "url": "/portfolio/static/media/bucheon.22f66603.jpg"
+  },
+  {
+    "revision": "98c90d82fbc0cdfc2bd14ab6e73fb340",
+    "url": "/portfolio/static/media/cat.98c90d82.gif"
+  },
+  {
+    "revision": "1496287abff3bddd42bbda37a82f824d",
+    "url": "/portfolio/static/media/employ.1496287a.jpg"
+  },
+  {
+    "revision": "3cc80f308b95ee18f1a57c5cdbc1013c",
+    "url": "/portfolio/static/media/escape.3cc80f30.png"
+  },
+  {
+    "revision": "00a2009f02e5148c646ed4883b6d951b",
+    "url": "/portfolio/static/media/movie.00a2009f.JPG"
+  }
+]);
